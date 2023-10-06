af353741efd31bc3a5fa30795b449dfeb9aa8b9d
KeaKzinho <145056753+KeaKzinho@users.noreply.github.com>
2023-10-06 15:00:45 -0300

    rpg
    RPG com múltiplas escolhas.

---
+++ rpg.py
@@ -0,0 +1,41 @@
+# História de "Garras e Unhas."
+
+from os import system
+
+# Tela de Boas-Vindas do usuário ----------------------------------------------------------------------------------------------------------------------
+system("cls")
+print("Olá meu amigo, antes da nossa aventura começar, qual o seu nome? ʕ•́ᴥ•̀ʔっ♡ ")
+nome = input()
+system("cls")
+print(f"{f'Olá {nome}, seja muito bem vindo(a) ao KeYa History!' : ^180}")
+print(f"{'Este é um RPG em produção, com primeiramente apenas uma história.' : ^180}")
+print(f"{'Futuramente irei adicionando cada vez mais, dependendo da minha força de vontade e determinação XD' : ^180}")
+print(f"{'Por mais é isso... Sem mais enrolação, vamos lá!' : ^180}")
+print(f"{'-' * 50 : ^180}")
+print(f"{'Jogo feito por KeaK, história feita por TLC e KeaK.' : ^180}")
+input(f"{'Aperte Enter para continuar.' : ^180}")
+system("cls")
+
+
+# Introdução (Cutscene)
+print(f"{'Garras e Unhas.' : ^180}")
+print(f"{'Brasil, 1990.' : ^180}")
+print()
+print("     A rivalidade entre humanos e ferais, sempre foi algo crescente e normalizado entre a sociedade... Mesmo com semelhanças, os humanos só apontavam as diferenças de acordo com suas")
+print("crenças, os chamados de bestas do demônio, pulgentos, selvagens e etc...")
+input()
+system("cls")
+print("Então, um certo dia, o destemido 'Clã Ak', o dono havia passado a liderança para seu filho, Kismo, onde faz um novo alistamento... Com uma regra nova, permitido ferais.\n")
+input("(Aperte Enter para continuar)")
+system("cls")
+
+# Começo do desenvolvimento -------------------------------------------------------------------------------------------------------------------------
+input("Kismo (Você) — Próximo!")
+input("[Chega um indivíduo de ali no mínimo 1,70 de altura com uma capa preta, que de certa forma era bem intimidador.]")
+input("Kismo (você) — Documentos e ficha de inscrição por favor.")
+input("? — [Apenas pega o papel, que estava todo amassado, e entrega às mãos diretamente do comandante.]\n")
+
+print(f"{'O que você irá fazer agora?' : ^180}")
+print(f"{'[ 1 ] - Apenas aceitar o papel de inscrição e verificar como se nada tivesse acontecido.' : ^180}")
+print(f"{'[ 2 ] - Pegar o papel de inscrição, amassar e chamar o próximo.' : ^180}")
+escolha_1 = int(input(""))
