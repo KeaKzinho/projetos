9bcf64b2eaf170eec30468e11130c08927e8b98b
KeaKzinho <145056753+KeaKzinho@users.noreply.github.com>
2023-10-18 07:46:03 -0300

    Atualizar o rpg.py

--- rpg.py
+++ rpg.py
@@ -1,4 +1,4 @@
-# História de "Garras e Unhas."
+O# História de "Garras e Unhas."
 
 from os import system
 
@@ -83,7 +83,7 @@ while True:
     system("cls")
 
     if opcao1 == 1:
-        input("Kauan - Mas que caralho de ficha é essa?! Alguns campos estão incompletos ou com respostas onde não posso aceitar!")
+        input("Kauan - Mas que caralho de ficha é essa?! Alguns campos estão incompletos, claro que não vou aceitar!")
         input("Ele não reage contra você, porém a forma como ele te enxerga muda drasticamente.")
 
     elif opcao1 == 2:
