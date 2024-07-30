b1a201d624038e22df8ee4522a00103ec70ac917
Keven Martins Ferraz Faria <145056753+KeaKzinho@users.noreply.github.com>
2024-07-30 11:12:14 -0300

    Delete Calculadora HTML directory

--- Calculadora HTML/a.js
+++
@@ -1,6 +0,0 @@
-function calcularSoma(){
-      let num1 = Number(numero1.value)
-      let num2 = Number(numero2.value)
-      let res = num1+num2
-      resultado.innerHTML = `O resultado da soma ${res}`
-}
